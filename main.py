934a25ea5bfda5baa031f31288d28d6375eca5c7
1C46US <niem4nd369@proton.me>
2025-10-06 23:39:56 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import socket
+from colorama import init, Fore
+
+
+init()
+GREEN = Fore.GREEN
+RESET = Fore.RESET
+GRAY = Fore.LIGHTBLACK_EX
+
+
+def port_checker(host, port):
+    x = socket.socket()
+    x.settimeout(0.2)
+    try:
+        x.connect((host, port))
+    except:
+        return False
+    else:
+        return True
+
+
+# host = input("Enter the host ip here: ")
+host = "8.8.8.8"
+
+
+for port in range(1, 1024):
+    if port_checker(host, port):
+        print(f'{GREEN}[+]{host}:{port} is open  {RESET}')
+    else:
+        print(f'{GRAY}[!]{host}:{port} is closed  {RESET}')
